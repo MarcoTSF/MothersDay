18e2f88b622d7b04f54f23e85dda7616bd4946e3
Marco Túlio Salvador Filho <mtsf.salvador@gmail.com>
2023-05-13 22:19:26 -0300

    Update links

--- src/js/link-gallery.js
+++ src/js/link-gallery.js
@@ -1,4 +1,4 @@
 const galleryBtn = document.getElementById('gallery-button');
 galleryBtn.addEventListener('click', function() {
-  window.location.href = '../MothersDay/src/pages/gallery.html';
+  window.location.href = 'https://marcotsf.github.io/MothersDay/src/pages/gallery.html';
 });
